dd4cef945a40b74d18a5420da9399808f0b31e47
natalia <nataliav@gmail.com>
2020-01-05 18:00:14 -0500

    Added missing code for lesson 18

--- lessons/how-to-use-transient-transitions/before.js
+++ lessons/how-to-use-transient-transitions/before.js
@@ -1,1 +1,27 @@
-// before.js
+const { assign, Machine } = require('xstate')
+
+const ifAtFirstYoudontSucceed = Machine(
+  {
+    id: 'tryTryAgain',
+    initial: 'idle',
+    context: {
+      tries: 0,
+    },
+    states: {
+      idle: {
+        on: { TRY: 'trying' },
+      },
+      trying: {
+        entry: ['incTries'],
+      },
+      success: {},
+    },
+  },
+  {
+    actions: {
+      incTries: assign({
+        tries: ctx => ctx.tries + 1,
+      }),
+    },
+  }
+)
